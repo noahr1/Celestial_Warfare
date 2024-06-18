2b4792111de19e4050c76cf4a5a092a374d702cd
Noah Reynolds <betajsion@gmail.com>
2024-06-17 22:08:38 -0400

    Adding file directory, and initial files

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Celestial_Warfare
 The cooler and more functional version of the solar battles
+
+## Game Dependencies:
+- JS
+- P5.js
+
---
+++ scripts/client/main.js
@@ -0,0 +1,19 @@
+//imports
+
+//global variables
+let socket;
+
+
+
+//initialization
+function setup() {
+
+}
+
+
+//primary rendering
+function draw() {
+
+}
+
+//server communication
